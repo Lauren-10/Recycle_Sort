321d04003c672bb8e7696a2e63b8d02612e2ca98
Lauren Knopp <lauren.knopp0@gmail.com>
2024-11-17 11:18:47 -0500

    prompt done

---
+++ scripts/prompt.py
@@ -0,0 +1,42 @@
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    FewShotChatMessagePromptTemplate,
+)
+from langchain_openai import ChatOpenAI
+
+def prompt():
+    
+    # This is a prompt template used to format each individual example.
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+        ]
+    )
+    
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=[]
+    )
+    
+    #want to return final prompt
+    #print(few_shot_prompt.format())
+    final_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", """You are an AI assistant designed to take in photos of items to be recycled or placed into trash.
+            You will given a description of the object to be recycled/trashed and classify whether or not the item can be placed to trash or recycling.
+            If the item has a clear label, utilize your knowledge base to decide whether or not that product is recyclable
+            If the item does not have a cleaar label (ie. candy wrapper, glass bottle, newspaper, etc.), make an educated guess as to what category the item is in.
+            Once you have completed your classification, write true if the item is recyclable, false otherwise
+            """),
+            few_shot_prompt,
+            ("human", "{input}"),
+        ]
+    )
+    return final_prompt
+
+
+
+chain = prompt() | ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+
+print(chain.invoke({"input": """yerba mate can"""}).content)
