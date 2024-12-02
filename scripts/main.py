661e4227ab1ec786aa632d06f972adefd245272c
Lauren Knopp <lauren.knopp0@gmail.com>
2024-12-02 16:36:47 -0500

    finalized

--- scripts/main.py
+++ scripts/main.py
@@ -4,27 +4,12 @@ from langchain_core.prompts import (
     FewShotChatMessagePromptTemplate,
 )
 from langchain_openai import ChatOpenAI
-
+import RPi.GPIO as GPIO
+import time
 
 #chain the prompt to an OpenAi model
 chain = prompt() | ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
 
-item = input("Enter a description of the item: ")
-
-instruction = eval(chain.invoke({"input": item}).content)
-#get the response from the prompt:
-
-
-#get input prompt from user
-if(instruction):
-    print("Spin the Recycling Bin")
-else:
-    print("Spin the Trash Bin")
-
-
-
-import RPi.GPIO as GPIO
-import time
 
 PIN = 18
 PIN_Y = 22
@@ -35,9 +20,10 @@ GPIO.setup(PIN, GPIO.OUT)
 GPIO.setup(PIN_Y, GPIO.OUT)
 
 next_decision = input("Enter a description of the item: ")
-recyclables = ["yerba mate can", "water bottle", "newspaper", ]
+instruction = eval(chain.invoke({"input": next_decision}).content)
+
 while not next_decision == "done":
-    if next_decision in recyclables:
+    if instruction is True:
         GPIO.output(PIN, GPIO.HIGH)
         time.sleep(5)
         print("That item is recylable!")
@@ -49,4 +35,7 @@ while not next_decision == "done":
         GPIO.output(PIN_Y, GPIO.LOW)
     
     next_decision = input("Enter a description of the item: ")
+    instruction = eval(chain.invoke({"input": next_decision}).content)
+
+
 
